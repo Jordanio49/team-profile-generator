fe8cb17c0c76bbcc2aae755bec9726836daa2c72
Jordan Thomas <jthomas135@hotmail.com>
2022-05-25 17:58:49 -0700

    added employee prompts. need to figure out proper email validation

--- index.js
+++ index.js
@@ -34,15 +34,18 @@ const addManager = () => {
             name: 'id',
             message: 'What is your employee id?',
             validate: idInput => {
-                if (idInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your employee id.');
+                if (isNaN(idInput)) {
+                    console.log('Please enter valid id number.');
                     return false;
+                } else {
+                    return true;
                 }
             }
         },
         {
+            // important note to self!
+            // how do you validate emails to make sure its an actually valid address instead of any old string?
+            // important note to self!
             type: 'input',
             name: 'email',
             message: 'What is your preferred email address?',
@@ -78,4 +81,87 @@ const addManager = () => {
     })
 };
 
+const addEmployee = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: 'What role will this employee play?',
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of this employee?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the employees's name.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the id for this employee?',
+            validate: idInput => {
+                if (isNaN(idInput)) {
+                    console.log('Please enter valid id number.');
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            // important note to self!
+            // how do you validate emails to make sure its an actually valid address instead of any old string?
+            // important note to self!
+            type: 'input',
+            name: 'email',
+            message: 'What is the preferred email for this employee?',
+            validate: emailInput => {
+                if(emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a valid email address.');
+                    return false;
+                }
+            }
+        },
+        {
+            // using when to only make this prompt appear if the user selected the 'Engineer' role
+            type: 'input',
+            name: 'github',
+            message: "What is this employee's github username?",
+            when: (input) => input.role === 'Engineer',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a github username.');
+                    return false;
+                }
+            }
+        },
+        {
+            // using when to only make this prompt appear if the user selected 'Intern' role
+            type: 'input',
+            name: 'school',
+            message: 'What school is this employee affiliated with?',
+            when: (input) => input.role === 'Intern',
+            validate: schoolInput => {
+                if (schoolInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a school name');
+                    return false;
+                }
+            }
+        }
+    ])
+}
 addManager();
+// addEmployee();
